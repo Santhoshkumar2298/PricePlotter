a349fb101ee83c7fc9cb0531db9bd4b3250f83fa
Santhoshkumar2298 <rangasan221998@gmail.com>
2024-04-11 08:50:56 +0530

    Replace Selenium with BeautifulSoup

--- controller.py
+++ controller.py
@@ -1,50 +1,79 @@
 import os
 import time
 from dotenv import load_dotenv
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options as EdgeOptions
 import smtplib
+import requests
 
 load_dotenv()
 MY_EMAIL = os.getenv('MY_EMAIL')
 MY_PASSWORD = os.getenv('MY_PASSWORD')
 
 
+# def check_valid_url(url, site):
+#     edge_options = EdgeOptions()
+#     edge_options.add_argument("--headless")
+#     edge_options.add_argument('--enable-javascript')
+#     edge_options.add_experimental_option("detach", True)
+#     edge_options.use_chromium = True
+#     driver = webdriver.Edge(edge_options)
+#     driver.get(url)
+#
+#     if site.lower() == "amazon":
+#         try:
+#             time.sleep(3)
+#             price = driver.find_element(By.CLASS_NAME, 'a-price-whole')
+#             price = price.text.replace(",", "")
+#             return price
+#         except Exception as e:
+#             print(str(e))
+#             return False
+#         except TimeoutError:
+#             return "TimeOut"
+#
+#     elif site.lower() == "flipkart":
+#         try:
+#             time.sleep(3)
+#             price = driver.find_element(By.CLASS_NAME, '_30jeq3')
+#             stripped_price = price.text.strip("₹")
+#             stripped_price = stripped_price.replace(",", "")
+#             return stripped_price
+#         except Exception as e:
+#             print(str(e))
+#             return False
+#         except TimeoutError:
+#             return "TimeOut"
+#     driver.quit()
+
 def check_valid_url(url, site):
-    edge_options = EdgeOptions()
-    edge_options.add_argument("--headless")
-    edge_options.add_argument('--enable-javascript')
-    edge_options.add_experimental_option("detach", True)
-    edge_options.use_chromium = True
-    driver = webdriver.Edge(edge_options)
-    driver.get(url)
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
+    response = requests.get(url, headers=headers)
 
     if site.lower() == "amazon":
-        try:
-            time.sleep(3)
-            price = driver.find_element(By.CLASS_NAME, 'a-price-whole')
-            price = price.text.replace(",", "")
-            return price
-        except Exception as e:
-            print(str(e))
+        time.sleep(5)
+        soup = BeautifulSoup(response.content, 'html5lib')
+        price = soup.find('span', attrs={'class': 'a-price-whole'})
+        if price is not None:
+            price_text = price.get_text(strip=True)
+            return price_text.strip(".")
+        else:
             return False
-        except TimeoutError:
-            return "TimeOut"
 
     elif site.lower() == "flipkart":
-        try:
-            time.sleep(3)
-            price = driver.find_element(By.CLASS_NAME, '_30jeq3')
-            stripped_price = price.text.strip("₹")
-            stripped_price = stripped_price.replace(",", "")
+        time.sleep(5)
+        soup = BeautifulSoup(response.content, 'html5lib')
+        price = soup.find('div', attrs={'class': '_30jeq3 _16Jk6d'})
+        if price is not None:
+            price_text = price.get_text(strip=True)
+            stripped_price = price_text.strip("₹").replace(",", "")
             return stripped_price
-        except Exception as e:
-            print(str(e))
+        else:
             return False
-        except TimeoutError:
-            return "TimeOut"
-    driver.quit()
 
 
 def mail_price_alert(price, email, product_name):
