2f410d01deb1b05bc4758a1776c883c96561c277
Santhoshkumar2298 <rangasan221998@gmail.com>
2024-04-11 12:25:37 +0530

    Debugging

--- controller.py
+++ controller.py
@@ -75,8 +75,6 @@ def check_valid_url(url, site):
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
             'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-            'Origin': "https://www.flipkart.com/",
-            'Referer': "https://www.flipkart.com/"
         }
         response = requests.get(url, headers=headers)
         logger.info(response.content)
